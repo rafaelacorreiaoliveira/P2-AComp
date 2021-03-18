820746e2ae390c298a22ef28b6aa66c3bf779f47
Rafaela Oliveira <80894252+rafaelacorreiaoliveira@users.noreply.github.com>
2021-03-18 11:42:02 +0000

    Add files via upload

---
+++ 1b).s
@@ -0,0 +1,6 @@
+li x10,0x0003
+li x11,0x0007
+li x12,0x0004
+add x11,x11,x10
+sub x11,x11,x12
+sll x10,x11,x10
---
+++ 1c).s
@@ -0,0 +1,5 @@
+li x10, 0x0338
+li x11, 0x0004
+li x13, 0x000f
+srl x11, x10, x11
+and x13, x11, x13
